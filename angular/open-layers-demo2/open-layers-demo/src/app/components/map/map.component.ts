23ba9d812e15e0b233cfea51bb5b72336af54896
Jonathan Lee <lee.jonathan.p@gmail.com>
2025-03-02 17:14:21 -0500

    progress on using open layers in angular

---
+++ angular/open-layers-demo2/open-layers-demo/src/app/components/map/map.component.ts
@@ -0,0 +1,24 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
+import Map from 'ol/Map';
+
+@Component({
+  selector: 'app-map',
+  template: '',
+  styles: [':host { width: 100%; height: 100%; display: block; }'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MapComponent implements OnInit {
+  @Input() map!: Map;
+  constructor(private elementRef: ElementRef) {}
+  ngOnInit() {
+    this.map.setTarget(this.elementRef.nativeElement);
+  }
+  
+}
---
+++ angular/open-layers-demo2/open-layers-demo/src/app/components/mouse-position/mouse-position.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+  ElementRef,
+} from '@angular/core';
+import Map from 'ol/Map';
+import ControlMousePosition from 'ol/control/MousePosition';
+import { CoordinateFormatterService } from '../../services/coordinate-formatter.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-mouse-position',
+  template: ``,
+  styles: [],
+  standalone: true,
+  imports: [CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MousePositionComponent implements OnInit {
+  @Input() map!: Map;
+  @Input() positionTemplate!: string;
+  control!: ControlMousePosition;
+
+  constructor(
+    private element: ElementRef,
+    private coordinateFormatter: CoordinateFormatterService
+  ) {}
+
+  ngOnInit() {
+    this.control = new ControlMousePosition({
+      className: 'mouseposition-control',
+      coordinateFormat: (coordinates: number[] | undefined) => {
+        if (coordinates) {
+          return this.coordinateFormatter.numberCoordinates(
+            coordinates,
+            4,
+            this.positionTemplate
+          );
+        }
+        else {
+          return '';
+        }
+      },
+        
+      target: this.element.nativeElement,
+    });
+    this.map.addControl(this.control);
+  }
+}
